0bcbb944759cbc4ad9e8df4375ef60dacca86fa6
Deyuan Deng <deyuan.deng@gmail.com>
2014-06-06 18:36:14 -0400

    Emacs js and js2 mode.

--- .emacs.d/inits/init-js2-mode.el
+++ .emacs.d/inits/init-js2-mode.el
@@ -7,6 +7,9 @@
 
 (defun js2-mode-custom-hook ()
   (setq js2-basic-offset 2))
+(defun js-mode-custom-hook ()
+  (setq js-indent-level 2))
+
 
 ;; Use js2-mode as major mode for javascript.
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
@@ -14,6 +17,7 @@
 (add-to-list 'interpreter-mode-alist '("node" . js2-mode))
 ;; Add custom hook.
 (add-hook 'js2-mode-hook 'js2-mode-custom-hook)
+(add-hook 'js-mode-hook 'js-mode-custom-hook)
 
 
 (provide 'init-js2-mode)
