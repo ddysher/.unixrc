47d6d834b2df21dea2616fdace43639874f15885
Deyuan Deng <deyuan@google.com>
2014-06-10 15:27:51 -0400

    Add revert-all-buffers

--- .emacs.d/inits/init-functions.el
+++ .emacs.d/inits/init-functions.el
@@ -45,5 +45,16 @@
   (balance-windows)
   (enlarge-window 15))
 
+(defun revert-all-buffers ()
+  "Refreshes all open buffers from their respective files."
+  (interactive)
+  (dolist (buf (buffer-list))
+    (with-current-buffer buf
+      (when (and (buffer-file-name)
+                 (file-exists-p (buffer-file-name))
+                 (not (buffer-modified-p)))
+        (revert-buffer t t t) )))
+  (message "Refreshed open files."))
+
 
 (provide 'init-functions)
