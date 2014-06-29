de5be5a8d9e72a3b5b5bfe35610c1dd4ad48f04f
Deyuan Deng <deyuan.deng@gmail.com>
2014-06-29 16:59:52 -0400

    Add tern mode for javascript autocomplete.

--- .emacs.d/inits/init-js2-mode.el
+++ .emacs.d/inits/init-js2-mode.el
@@ -2,13 +2,15 @@
 ;; Provide javascript mode, javascript-mode is built-in emacs mode
 ;;------------------------------------------------------------------------------
 (require-package 'js2-mode)
+(require-package 'tern)
+(require-package 'tern-auto-complete)
 (require 'js2-mode)
 
 
 (defun js2-mode-custom-hook ()
-  (setq js2-basic-offset 2))
+  (setq js2-basic-offset universal-indent-size))
 (defun js-mode-custom-hook ()
-  (setq js-indent-level 2))
+  (setq js-indent-level universal-indent-size))
 
 
 ;; Use js2-mode as major mode for javascript.
@@ -18,6 +20,11 @@
 ;; Add custom hook.
 (add-hook 'js2-mode-hook 'js2-mode-custom-hook)
 (add-hook 'js-mode-hook 'js-mode-custom-hook)
+(add-hook 'js2-mode-hook (lambda () (tern-mode t)))
+(eval-after-load 'tern
+   '(progn
+      (require 'tern-auto-complete)
+      (tern-ac-setup)))
 
 
 (provide 'init-js2-mode)
