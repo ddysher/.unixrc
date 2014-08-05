c0fcc27879855d864f63cf842c3c832dd78576a8
Deyuan Deng <deyuan@google.com>
2014-08-05 14:40:24 -0400

    Web mode indentation and offset.

--- .emacs.d/inits/init-web-mode.el
+++ .emacs.d/inits/init-web-mode.el
@@ -5,6 +5,7 @@
 (require 'web-mode)
 
 
+;; Associate theses files with web-mode.
 (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.[gj]sp\\'" . web-mode))
@@ -15,6 +16,17 @@
 (add-to-list 'auto-mode-alist '("\\.ejs\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.css\\'" . web-mode))
+
+(defun web-mode-custom-hook ()
+  ;; Customize indentations (indentation inside code).
+  (setq web-mode-markup-indent-offset universal-indent-size) ; HTML
+  (setq web-mode-css-indent-offset universal-indent-size)    ; CSS
+  (setq web-mode-code-indent-offset universal-indent-size)   ; Script
+  ;; Customize padding.
+  (setq web-mode-style-padding 2)       ; For <style> parts
+  (setq web-mode-script-padding 2))     ; for <script> parts
+
+(add-hook 'web-mode-hook 'web-mode-custom-hook)
 
 
 (provide 'init-web-mode)
