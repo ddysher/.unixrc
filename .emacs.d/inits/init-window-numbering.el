48cb72d5dca0c10f0def09be77a4f552e62b83e2
Deyuan Deng <deyuan.deng@gmail.com>
2014-08-13 19:26:24 -0400

    do not map window number 4

--- .emacs.d/inits/init-window-numbering.el
+++ .emacs.d/inits/init-window-numbering.el
@@ -8,8 +8,6 @@
 (defun window-numbering-mode-custom-hook (windows)
   (let ((counter 1))
     (dolist (window windows)
-      (if (equal counter 4)
-          (window-numbering-assign window 8))
       (if (equal counter 5)
           (window-numbering-assign window 0))
       (if (equal counter 6)
