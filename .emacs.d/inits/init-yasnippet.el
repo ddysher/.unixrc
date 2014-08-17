9f35ae3d9f6d774a29bd8291c3cd9ed0cb410124
Deyuan Deng <deyuan.deng@gmail.com>
2014-08-17 13:30:09 -0400

    Some snippet

--- .emacs.d/inits/init-yasnippet.el
+++ .emacs.d/inits/init-yasnippet.el
@@ -1,12 +1,20 @@
 ;;------------------------------------------------------------------------------
-;; Provide yasnippet mode, a template system for Emacs. It allows you to type an
-;; abbreviation and automatically expand it into function templates. It's elpa
-;; managed mode.
-;;   Use (yas-global-mode 1) to enable yasnippet globally; or use (yas-reload-all)
+;; Provide yasnippet mode, managed by MELPA.  Yasnippet mode is a template
+;; system for Emacs. It allows you to type an abbreviation and automatically
+;; expand it into function templates.
+;;
+;; Configuration:
+;;   Use (yas-global-mode 1) to enable yasnippet globally; or (yas-reload-all)
 ;;     to load snippets table, then add (yas-minor-mode) to major mode hook.
-;;   Use yas-snippet-dirs to control which directories to load snippets table.
-;;     Default directory is elpa's yasnippet installation directory, also
+;;   Use variable yas-snippet-dirs to control which directories to load snippets
+;;     table. Default directory is melpa's yasnippet installation directory, and
 ;;     snippets under ~/.emacs.d/snippets.
+;;
+;; Usage:
+;;   C-o : Expand yasnippet. Originally, yas-expand is bound to TAB, but it
+;;     doesn't work well with auto-complete; this file rebinds it to C-o.
+;;   M-x yas-reload-all: Reload all yasnippet, useful when new snippet is added
+;;     in current emacs session.
 ;;------------------------------------------------------------------------------
 (require-package 'yasnippet)
 (require 'yasnippet)
