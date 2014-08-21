5799c003dbbb78cd69b11bf4ff8144c7a85b56fc
Deyuan Deng <deyuan.deng@gmail.com>
2014-08-21 19:52:03 -0400

    Refactor install script.

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -6,9 +6,8 @@ set +x
 # Package versions
 #
 GO_VERSION="1.3"
-NODE_VERSION="v0.10.29"       # npm version v1.4.14, shipped with node
+NODE_VERSION="v0.10.31"
 EMACS_VERSION="dev"
-THRIFT_VERSION="dev"
 MONGODB_VERSION="2.6.3"
 VAGRANT_VERSION="1.6.3"
 
@@ -28,33 +27,70 @@ VAGRANT_PACKAGE="vagrant_${VAGRANT_VERSION}_x86_64.deb"
 VAGRANT_URL="https://dl.bintray.com/mitchellh/vagrant/$VAGRANT_PACKAGE"
 
 
+#
+# Entry point
+# NOTE: the installation order matters.
+#
+function InstallAll() {
+
+  if [[ $USER = "root" ]]; then
+    echo "Do not run as root, configuration depends on user name."
+    exit
+  fi
+
+  # Install system packages to /usr/.
+  InstallSystemPkg
+
+  # Install packages to /usr/local/.
+  git submodule init
+  git submodule update
+  InstallThirdPartyPkg
+  InstallEmacs
+  InstallGo
+  InstallNodeJs
+  InstallMongoDB
+  InstallVagrant
+  InstallDocker
+  InstallKubernetes
+
+  # Setup environment and clean up.
+  SetupEnvironment
+  CleanUp
+}
+
+
 function InstallSystemPkg() {
   # Update packages
   sudo apt-get update
   # Basic tools
-  sudo apt-get install -y git wget curl zsh mercurial build-essential \
-       exfat-fuse exfat-utils
+  sudo apt-get install -y \
+       git wget curl zsh mercurial build-essential exfat-fuse exfat-utils
   # Build tools
-  sudo apt-get install -y cmake automake libtool
+  sudo apt-get install -y \
+       cmake automake libtool
   # Customization tools
-  sudo apt-get install -y gnome-tweak-tool ttf-wqy-zenhei fonts-inconsolata \
-       compizconfig-settings-manager compiz-plugins-extra wmctrl
+  sudo apt-get install -y \
+       gnome-tweak-tool ttf-wqy-zenhei fonts-inconsolata wmctrl \
+       compizconfig-settings-manager compiz-plugins-extra
   # Language tools
-  sudo apt-get install -y markdown python-pip php5 php5-mysql php5-gd php5-dev \
-       php5-curl php-apc php5-cli php5-json python-dev g++ libglib2.0-dev \
+  sudo apt-get install -y \
+       markdown python-pip php5 php5-mysql php5-gd php5-dev php5-curl \
+       php-apc php5-cli php5-json python-dev g++ libglib2.0-dev \
        libevent-dev meld lua5.2
   # Install required packages for emacs.
-  sudo apt-get install -y texinfo libxpm-dev libpng-dev libgif-dev \
-       libjpeg-dev libtiff-dev libgtk-3-dev libncurses5-dev
-  # Other tools
-  sudo apt-get w3m
+  sudo apt-get install -y \
+       texinfo libxpm-dev libpng-dev libgif-dev libjpeg-dev libtiff-dev \
+       libgtk-3-dev libncurses5-dev w3m
+}
+
+
+function InstallThirdPartyPkg() {
   sudo pip install ipython
   sudo pip install pylint
 }
 
 
 function InstallEmacs() {
-  # Build from source (emacs is a git-submodule).
   cd tools/emacs
   ./autogen.sh
   ./configure
@@ -91,23 +127,31 @@ function InstallMongoDB() {
 }
 
 
-function InstallThrift() {
-  # Basic build requirements and language support
-  sudo apt-get install -y libboost-dev libboost-test-dev flex bison pkg-config \
-       libboost-program-options-dev libssl-dev
-  if [[ ! -e thrift ]]; then
-    git clone https://git-wip-us.apache.org/repos/asf/thrift.git thrift
-  fi
-  # Build from source.
-  cd thrift
-  if [[ $THRIFT_VERSION != "dev" ]]; then
-    git checkout tags/$THRIFT_VERSION
-  fi
-  ./bootstrap.sh
-  ./configure
-  make
-  sudo make install
-  cd -
+function InstallVagrant() {
+  wget $VAGRANT_URL
+  sudo dpkg -i $VAGRANT_PACKAGE
+}
+
+
+function InstallDocker() {
+  # Only works for ubuntu >= 14.04
+  sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
+  sudo sh -c "echo deb https://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
+  sudo apt-get update
+  sudo apt-get install lxc-docker
+}
+
+
+function InstallKubernetes() {
+  export GOPATH=$HOME/code/source/go-workspace
+  go get github.com/coreos/etcd
+  go get github.com/tools/godep
+  go get github.com/nsf/gocode
+  go get code.google.com/p/rog-go/exp/cmd/godef
+  go get code.google.com/p/go.tools/cmd/goimports
+  # Link these binaries since we need to run some k8s scripts as root.
+  sudo ln -sf $GOPATH/bin/etcd /usr/bin/etcd
+  sudo ln -sf $GOPATH/bin/godep /usr/bin/godep
 }
 
 
@@ -151,71 +195,12 @@ function SetupEnvironment() {
 }
 
 
-function InstallKubernetes() {
-  export GOPATH=$HOME/code/source/go-workspace
-  go get github.com/coreos/etcd
-  go get github.com/tools/godep
-  go get github.com/nsf/gocode
-  go get code.google.com/p/rog-go/exp/cmd/godef
-  go get code.google.com/p/go.tools/cmd/goimports
-  # Link these binaries since we need to run some k8s scripts as root.
-  sudo ln -sf $GOPATH/bin/etcd /usr/bin/etcd
-  sudo ln -sf $GOPATH/bin/godep /usr/bin/godep
-}
-
-
-function InstallVagrant() {
-  wget $VAGRANT_URL
-  sudo dpkg -i $VAGRANT_PACKAGE
-}
-
-
-function InstallDocker() {
-  # Only works for > 14.04
-  sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
-  sudo sh -c "echo deb https://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
-  sudo apt-get update
-  sudo apt-get install lxc-docker
-}
-
-
 function CleanUp() {
   sudo rm -rf $NODE_PACKAGE $MONGODB_PACKAGE $GO_PACKAGE $VAGRANT_PACKAGE
-  sudo rm -rf thrift
   cd /usr/local
   sudo rm -rf ChangeLog GNU-AGPL-3.0 LICENSE README README.md THIRD-PARTY-NOTICES
   cd -
 }
 
 
-######################################################################
-#                         Start installation                         #
-######################################################################
-if [[ $USER = "root" ]]; then
-   echo "Do not run as root."
-   exit
-fi
-#
-# Install system packages to /usr/.
-#
-InstallSystemPkg
-
-#
-# Install packages to /usr/local/.
-#
-git submodule init
-git submodule update
-InstallEmacs
-InstallGo
-InstallNodeJs
-InstallMongoDB
-InstallThrift
-InstallVagrant
-InstallKubernetes
-InstallDocker
-
-#
-# Setup environment and clean up.
-#
-SetupEnvironment
-CleanUp
+InstallAll
