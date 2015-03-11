8e78f5cb31be9279d8113f8f7ef10a2fc8ec21c0
Deyuan Deng <deyuan@google.com>
2015-03-11 15:33:38 -0400

    Disable travis-ci and jenkens (pending kubot)

--- .emacs.d/inits/init-erc.el
+++ .emacs.d/inits/init-erc.el
@@ -12,8 +12,29 @@
 (setq erc-server "deyuan.me")
 
 (defun erc-mode-custom-hook ()
+  ;; 'erc-ignore-list' is a buffer local variable.
+  (setq erc-ignore-list '("*bot" "jenkins-k8s*" "travis*"))
   ;; (text-scale-decrease 1)               ; use smaller size for term
   (setq truncate-lines t) )
+
+
+(defcustom erc-foolish-content '("*pull request*")
+  "Regular expressions to identify foolish content.
+Usually what happens is that you add the bots to
+`erc-ignore-list' and the bot commands to this list."
+  :group 'erc
+  :type '(repeat regexp))
+
+;; Not working yet (to disable -[kubot]-
+(defun erc-foolish-content (msg)
+  "Check whether MSG is foolish."
+  (erc-list-match erc-foolish-content msg))
+
+(add-hook
+ 'erc-insert-pre-hook
+ (lambda (s)
+   (when (erc-foolish-content s)
+     (setq erc-insert-this nil))))
 
 (add-hook 'erc-mode-hook 'erc-mode-custom-hook)
 
