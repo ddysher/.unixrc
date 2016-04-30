d7d6a91d813407ec09ce5ee55f2a33912cc05772
Deyuan Deng <deyuan.deng@gmail.com>
2016-04-30 18:15:15 +0800

    Remove unused comment

--- .emacs.d/inits/init-helm.el
+++ .emacs.d/inits/init-helm.el
@@ -6,14 +6,8 @@
 (require 'helm)
 
 
-;; ;; Basic config from helm, copied from elpa/helm-xxxx/helm-config.el. Main
-;; ;; customization is to disable helm find files and use ido. Helm grep, etc
-;; ;; is enabled.
-;; (require 'helm-config)
-
 (helm-mode 1)
 (helm-adaptive-mode 1)
-
 
 (defun helm-do-grep-recursive ()
   "Like `helm-do-grep', but greps recursively by default and targeting current directory."
