6b273d26a45a0337a9be7a053136710fd8cdbeab
Deyuan Deng <deyuan@caicloud.io>
2019-04-04 21:56:08 +0800

    markdown live preview workaround

--- .emacs.d/inits/init-markdown-mode.el
+++ .emacs.d/inits/init-markdown-mode.el
@@ -14,6 +14,12 @@
   (revert-buffer t t)
   (message "markdown table of contents updated"))
 
+(defun markdown-live-preview ()
+  ;; kill livedown server first to avoid duplicate anchors
+  (interactive)
+  (livedown:kill)
+  (livedown:preview))
+
 (defun markdown-custom-hook()
   (run-hooks 'prog-mode-hook)
   (local-set-key (kbd "C-c C-c d") 'markdown-doctoc)
@@ -26,7 +32,7 @@
   (local-set-key (kbd "M-n") 'scroll-up-in-place)
   ;; Change 'C-M-m' key binding to open livedow preview. By default, it is
   ;;  bound to 'markdown-insert-list-item'.
-  (local-set-key (kbd "C-M-m") 'livedown:preview))
+  (local-set-key (kbd "C-M-m") 'markdown-live-preview))
 
 (add-hook 'markdown-mode-hook 'markdown-custom-hook)
 
