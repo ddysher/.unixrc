6e3b4d386e29ed329ced9bbbf99d15039d89f32e
Deyuan Deng <deyuan.deng@gmail.com>
2025-10-26 18:30:10 +0800

    python: update zsh to use uv

--- zshrc.d/development/python.zsh
+++ zshrc.d/development/python.zsh
@@ -1,8 +1,5 @@
 ##------------------------------------------------------------------------------
 ## Python Development Environment
-##
-## Additional one-time setup:
-## $ pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
 ##------------------------------------------------------------------------------
 
 # uv setup
@@ -11,61 +8,6 @@
 # use uv managed python globally.
 alias python="uv run python"
 alias pip="uv pip"
+alias venv="uv venv"
 
-
-# pyenv setup, used to manage multiple python versions (retiring).
-if [ -d "$HOME/.pyenv" ]; then
-  export PYENV_ROOT="$HOME/.pyenv"
-  export PATH="$PYENV_ROOT/bin:$PATH"
-  eval "$(pyenv init --path)"
-  eval "$(pyenv init -)"
-fi
-
-
-# pipx setup, used to install global python cli tools and applications
-if [[ -x $(command -v pipx) ]]; then
-  # Remove ipython alias set via zsh python plugin to avoid conflicts
-  unalias ipython 2>/dev/null || true
-  export PATH="$PATH:$HOME/.local/bin"
-fi
-
-
-# python virtual environment home.
-export VENV_HOME="$HOME/.venv"
-[[ -d $VENV_HOME ]] || mkdir -p $VENV_HOME
-
-# Virtual environment management functions
-function lsvenv() {
-  ls -1 "$VENV_HOME"
-}
-
-function venv() {
-  if [ $# -eq 0 ]; then
-    echo "Please provide venv name"
-  else
-    source "$VENV_HOME/$1/bin/activate"
-  fi
-}
-
-function mkvenv() {
-  if [ $# -eq 0 ]; then
-    echo "Please provide venv name"
-  else
-    echo "Creating venv under $VENV_HOME/$1"
-    python3 -m venv "$VENV_HOME/$1"
-    echo "Activating $1"
-    venv "$1"
-  fi
-}
-
-function rmvenv() {
-  if [ $# -eq 0 ]; then
-    echo "Please provide venv name"
-  else
-    rm -rf "$VENV_HOME/$1"
-  fi
-}
-
-# Completions for venv functions
-complete -C lsvenv venv
-complete -C lsvenv rmvenv
+export UV_DEFAULT_INDEX="https://pypi.tuna.tsinghua.edu.cn/simple"
