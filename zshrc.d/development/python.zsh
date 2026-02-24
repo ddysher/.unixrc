e8f5cb6557cb963f98f11960e09073500f8442b5
Deyuan Deng <deyuan.deng@gmail.com>
2026-02-24 08:38:25 +0800

    update python config: use uv --default

--- zshrc.d/development/python.zsh
+++ zshrc.d/development/python.zsh
@@ -5,9 +5,11 @@
 # uv setup
 [[ -f "$HOME/.local/bin/env" ]] && source "$HOME/.local/bin/env" # added by uv
 
-# use uv managed python globally.
-alias python="uv run python"
-alias pip="uv pip"
-alias venv="uv venv"
-
 export UV_DEFAULT_INDEX="https://pypi.tuna.tsinghua.edu.cn/simple"
+
+# add alias to use uv managed python globally.
+# note: disable and use "uv python install 3.11 --default"
+#
+# alias python="uv run python"
+# alias pip="uv pip"
+# alias venv="uv venv"
