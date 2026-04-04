1440375a0fbdb414dbd6f370804e064d35612336
Deyuan Deng <deyuan.deng@gmail.com>
2026-04-04 15:58:16 +0800

    setup proxy and remove outdated configs

--- zshrc.d/core/path.zsh
+++ zshrc.d/core/path.zsh
@@ -14,6 +14,7 @@ if [[ "$(uname)" == "Darwin" ]]; then
   export HOMEBREW_PREFIX="/opt/homebrew"
   export HOMEBREW_CELLAR="/opt/homebrew/Cellar"
   export HOMEBREW_REPOSITORY="/opt/homebrew"
+  export HOMEBREW_NO_ENV_HINTS=1
   export PATH="$HOMEBREW_PREFIX/bin:$HOMEBREW_PREFIX/sbin:$PATH"
   export MANPATH="$HOMEBREW_PREFIX/share/man:$MANPATH"
   export INFOPATH="$HOMEBREW_PREFIX/share/info${INFOPATH+:$INFOPATH}"
