ed074fb700e2543ae49ab1d10aebfa7799f21efb
Deyuan Deng <deyuan.deng@gmail.com>
2026-04-06 15:04:29 +0800

    Revert package-initialize removal and embark-consult demand

--- emacs.d/inits/init-elpa-packages.el
+++ emacs.d/inits/init-elpa-packages.el
@@ -7,6 +7,8 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (add-to-list 'package-archives  '("gnu" . "http://elpa.gnu.org/packages/"))
 
+(package-initialize)
+
 ;; Auto-refresh quickstart cache after installing or deleting packages.
 (advice-add 'package-install :after
             (lambda (&rest _) (package-quickstart-refresh)))
