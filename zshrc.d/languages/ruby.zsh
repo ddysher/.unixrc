37acd22aa7962cecc3b7104963b19c1b85439fd1
Deyuan Deng <dengdeyuan.dengdy@bytedance.com>
2026-04-15 22:39:35 +0800

    zsh: remove Ruby configuration

--- zshrc.d/languages/ruby.zsh
+++
@@ -1,9 +0,0 @@
-##------------------------------------------------------------------------------
-## Ruby Development Environment
-##------------------------------------------------------------------------------
-
-# rbenv setup
-if [[ -d "$HOME/.rbenv" ]]; then
-  export PATH="$PATH:$HOME/.rbenv/bin"
-  eval "$(rbenv init -)"
-fi
