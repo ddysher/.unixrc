855023140fa6be622c757443f9ba0573b98980bc
Deyuan Deng <dengdeyuan.dengdy@bytedance.com>
2026-04-16 09:10:32 +0800

    zsh: fix local outside function in kubectl.zsh

--- zshrc.d/tools/kubectl.zsh
+++ zshrc.d/tools/kubectl.zsh
@@ -3,7 +3,7 @@
 ##------------------------------------------------------------------------------
 
 if command -v kubectl &>/dev/null; then
-  local _kubectl_cache="$HOME/.cache/zsh/kubectl-completion-${ZSH_VERSION}.zsh"
+  _kubectl_cache="$HOME/.cache/zsh/kubectl-completion-${ZSH_VERSION}.zsh"
   if [[ ! -f "$_kubectl_cache" || $(command -v kubectl) -nt "$_kubectl_cache" ]]; then
     kubectl completion zsh > "$_kubectl_cache"
   fi
