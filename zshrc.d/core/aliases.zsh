131f3e9fd5858125e6a9b938828b0d3f7d7dbede
Deyuan Deng <dengdeyuan.dengdy@bytedance.com>
2026-04-16 09:10:37 +0800

    zsh: remove duplicate lg alias, replace function-in-alias antipattern

--- zshrc.d/core/aliases.zsh
+++ zshrc.d/core/aliases.zsh
@@ -9,7 +9,6 @@ alias rm="rm -i"
 
 # Enhanced ls
 alias ll="ls -lh"
-alias lg="ls -lh"
 
 # Directory navigation (global aliases so they work as arguments too, e.g., "mv abc ...")
 # AUTO_CD in shell.zsh makes bare ".."/".." act as cd
@@ -22,9 +21,6 @@ alias -g ......='../../../../..'
 alias ppj="python -mjson.tool"  # Beautify JSON output
 alias sgrep="grep -rnI -C3 --color=always"  # Colorful recursive grep
 
-# Create directory and cd into it
-alias mkcd='function _mkcd() { mkdir -p "$@" && cd "$@"; }; _mkcd'
-
 # Git aliases (similar to ohmyzsh git plugin)
 alias gst="git status"
 alias gco="git checkout"
@@ -33,22 +29,25 @@ alias gcmsg="git commit -m"
 alias gp="git push"
 alias gl="git pull"
 
+# Create directory and cd into it
+mkcd() { mkdir -p "$@" && cd "$@"; }
+
 # Docker
-alias docker-ip='function _dip() { docker inspect --format "{{ .NetworkSettings.IPAddress }}" "$1"; }; _dip'
-alias docker-pid='function _dpid() { docker inspect --format "{{ .State.Pid }}" "$1"; }; _dpid'
+docker-ip() { docker inspect --format "{{ .NetworkSettings.IPAddress }}" "$1"; }
+docker-pid() { docker inspect --format "{{ .State.Pid }}" "$1"; }
 
 # Platform-specific aliases
 case "$(uname)" in
   Darwin)
     alias chrome="open -a Google\ Chrome"
-    alias e="TERM=xterm-256color /Applications/Emacs.app/Contents/MacOS/Emacs -nw"
+    alias es="TERM=xterm-256color /Applications/Emacs.app/Contents/MacOS/Emacs -nw"
     alias eq="TERM=xterm-256color /Applications/Emacs.app/Contents/MacOS/Emacs -nw -Q"
     alias ec="/Applications/Emacs.app/Contents/MacOS/bin/emacsclient -n"
     alias emacs="/Applications/Emacs.app/Contents/MacOS/bin/emacsclient -n"
     ;;
   Linux)
     alias chrome="google-chrome"
-    alias e="TERM=xterm-256color /usr/local/bin/emacs -nw"
+    alias es="TERM=xterm-256color /usr/local/bin/emacs -nw"
     alias eq="TERM=xterm-256color /usr/local/bin/emacs -nw -Q"
     alias ec="/usr/local/bin/emacsclient -n"
     alias emacs="/usr/local/bin/emacsclient -n"
