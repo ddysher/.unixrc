708b0ca8b1052b46c5fa7afeffd0c5ad427d0289
Deyuan Deng <dengdeyuan.dengdy@bytedance.com>
2026-04-18 14:42:53 +0800

    Add M-[/M-] keybindings for multi-vterm prev/next

--- emacs.d/inits/init-multi-vterm.el
+++ emacs.d/inits/init-multi-vterm.el
@@ -15,6 +15,8 @@
   (define-key vterm-mode-map [f8] #'register-to-point)
   (define-key vterm-mode-map (kbd "C-o")  #'switch-to-buffer)
   (define-key vterm-mode-map (kbd "C-q")  #'vterm-copy-mode)
+  (define-key vterm-mode-map (kbd "M-[")  #'multi-vterm-prev)
+  (define-key vterm-mode-map (kbd "M-]")  #'multi-vterm-next)
   (define-key vterm-mode-map (kbd "S-<return>")
     (lambda () ; Shift+Return → insert newline instead of raw return (for TUI).
       (interactive)
