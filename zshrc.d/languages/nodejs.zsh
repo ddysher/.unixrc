d8d7ecfcc46bdcd89f8b179df70d39b6e403e28b
Deyuan Deng <dengdeyuan.dengdy@bytedance.com>
2026-04-19 07:51:22 +0800

    Use Homebrew for Node.js tools

---
+++ zshrc.d/languages/nodejs.zsh
@@ -0,0 +1,9 @@
+##------------------------------------------------------------------------------
+## Node.js Development Environment
+##------------------------------------------------------------------------------
+
+# Use brew to manage global nodejs.
+export PATH="/opt/homebrew/opt/node@24/bin:$PATH"
+
+# Set npm registry
+export NPM_CONFIG_REGISTRY="https://registry.npmmirror.com/"
